3db23e3c386e7be466cc612105a8d9dc8eabedb5
Sourav Bhattacharjee <sourav030.bd@gmail.com>
2023-01-11 21:07:43 +0600

    Update userInfo class signature

--- lib/model/user_info.dart
+++ lib/model/user_info.dart
@@ -23,21 +23,9 @@ class UserInfo {
   double? height;
   List<String>? diseases;
 
-  factory UserInfo.fromRawJson(String str) => UserInfo.fromJson(json.decode(str));
+  factory UserInfo.fromRawJson(String str) => UserInfo.fromMap(json.decode(str));
 
-  String toRawJson() => json.encode(toJson());
-
-  factory UserInfo.fromJson(Map<String, dynamic> json) => UserInfo(
-    userId: json["user_id"],
-    fullName: json["full_name"],
-    age: json["age"],
-    gender: json["gender"],
-    jobType: json["job_type"],
-    designation: json["designation"],
-    weight: json["weight"],
-    height: json["height"],
-    diseases: List<String>.from(json["diseases"].map((x) => x)),
-  );
+  String toRawJson() => json.encode(toMap());
 
   factory UserInfo.fromMap(Map<String, dynamic> json) => UserInfo(
     userId: json["user_id"],
@@ -50,18 +38,6 @@ class UserInfo {
     height: json["height"],
     diseases: List<String>.from(json["diseases"].map((x) => x)),
   );
-
-  Map<String, dynamic> toJson() => {
-    "user_id": userId,
-    "full_name": fullName,
-    "age": age,
-    "gender": gender,
-    "job_type": jobType,
-    "designation": designation,
-    "weight": weight,
-    "height": height,
-    "diseases": diseases != null ? List<dynamic>.from(diseases!.map((x) => x)) : null,
-  };
 
   Map<String, dynamic> toMap() => {
     "user_id": userId,
