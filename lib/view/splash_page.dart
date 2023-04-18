e8804e89f195bc1cfbed5b05259974bec7d9973b
Sourav Bhattacharjee <sourav030.bd@gmail.com>
2023-04-19 00:47:32 +0600

    Fix task alert page issue for exercise list loading

--- lib/view/splash_page.dart
+++ lib/view/splash_page.dart
@@ -2,15 +2,11 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:app/main.dart';
-import 'package:app/service/task_alert_service.dart';
 import 'package:app/util/app_constant.dart';
 import 'package:app/util/app_style.dart';
 import 'package:app/util/data_loader.dart';
 import 'package:app/util/shared_preference.dart';
-import 'package:app/view/task_alert_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -49,17 +45,7 @@ class _SplashPageState extends State<SplashPage> {
     }
 
     if (isUserExist && mounted) {
-      final NotificationAppLaunchDetails? notDetails = await TaskAlertService.instance.getNotificationAppLaunchDetails();
-      if (notDetails != null && notDetails.didNotificationLaunchApp) {
-        print("notificationAppLaunchDetails: $notDetails");
-        String payload = notDetails.notificationResponse?.payload ?? "2";
-        int taskType = int.tryParse(payload) ?? TaskType.exercise.index;
-
-        print("-------> opening task alert page from _goToNextPage in splash page");
-        Navigator.pushNamed(navigatorKey.currentState!.context, taskAlertRoute, arguments: taskType);
-      } else {
-        Navigator.pushNamedAndRemoveUntil(navigatorKey.currentState!.context, landingRoute, (r) => false);
-      }
+      Navigator.pushNamedAndRemoveUntil(navigatorKey.currentState!.context, landingRoute, (r) => false);
     } else {
       Navigator.pushNamedAndRemoveUntil(navigatorKey.currentState!.context, userInfoRoute, (r) => false);
     }
