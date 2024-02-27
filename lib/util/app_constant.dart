8f41029d26af63cf2b07ea1dea411962db788693
Sourav Bhattacharjee <sourav030.bd@gmail.com>
2024-02-27 15:54:06 +0100

    Create new team for PMD

--- lib/util/app_constant.dart
+++ lib/util/app_constant.dart
@@ -7,7 +7,8 @@ class AppConstant {
   static const productionBuild = false;
   // static const baseURL = "http://10.0.2.2:8075"; // local
   static const baseURL = "https://kontikat.de/cardafit"; // live
-  static const teamNameForCustomBuild = 'bgf'; // bgf-internal, unisiegen-thesis, dokuworks-company1, maipham-mai
+  // bgf-internal, unisiegen-thesis, dokuworks-company1, pmd-company2, maipham-mai
+  static const teamNameForCustomBuild = 'bgf';
 
   static const waterTaskValue = 1;
   static const exerciseTaskValue = 3;
