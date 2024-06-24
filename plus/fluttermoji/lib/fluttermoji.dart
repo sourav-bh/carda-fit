35ea838389d9d404a6c513e3adc59b935f5e4742
maiphm <pmaipoc@gmail.com>
2024-06-24 19:41:10 +0200

    deleted duplicated folder

--- plus/fluttermoji/lib/fluttermoji.dart
+++
@@ -1,8 +0,0 @@
-library fluttermoji;
-
-export './fluttermojiCustomizer.dart';
-export './fluttermojiCircleAvatar.dart';
-export './fluttermojiController.dart';
-export './fluttermojiFunctions.dart';
-export './fluttermojiSaveWidget.dart';
-export './fluttermojiThemeData.dart';
